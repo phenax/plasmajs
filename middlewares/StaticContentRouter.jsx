7150a6c989348b2878d72ec7ecede69dd4244879
Akshay Nair <phenax5@gmail.com>
2016-11-01 14:56:56 +0530

    Added static files middleware

--- middlewares/StaticContentRouter.jsx
+++ middlewares/StaticContentRouter.jsx
@@ -8,12 +8,23 @@ export class StaticContentRouter extends MiddleWare {
 
 	onRequest(req, res) {
 
-		this.publicDir= this.props.public || 'static';
+		this.publicDir= this.props.dir || 'static';
+		this.hasPrefix= (this.props.hasPrefix == null)? true: this.props.hasPrefix;
 
-		if(this.props.hasPrefix) {
 
-			if(req.url.split('/')[1] === this.publicDir)
+		// If the directory name if to be prefixed i.e. 
+		// ./public will be hosted as http://domain.com/public/file
+		// instead of
+		// http://domain.com/file
+		if(this.hasPrefix) {
+
+			const publicPathRegex= new RegExp('^\/' + this.publicDir + '\/');
+
+			// If the url starts with /${publicDir}/
+			if(publicPathRegex.test(req.url)) {
+				console.log("public start");
 				this.sendFileContents();
+			}
 
 		} else {
 			this.sendFileContents();
@@ -21,8 +32,16 @@ export class StaticContentRouter extends MiddleWare {
 	}
 
 	fetchFileContents() {
+		const projectDir= path.resolve('.');
+		console.log(path.resolve(projectDir, './'+this.props.request.url ));
+		console.log(path.resolve(projectDir, this.publicDir + '/' + this.props.request.url ));
 
-		return fs.readFileSync(path.resolve('.' + this.props.request.url));
+		// TODO: Set for prefixes
+		return fs.readFileSync(
+			(this.hasPrefix) ?
+				path.resolve(projectDir, './'+this.props.request.url ) :                     // Has prefix i.e. static content url will be /${publicDir}/whatever
+				path.resolve(projectDir, this.publicDir + '/' + this.props.request.url )     // No prefix i.e. static content url will be  /whatever
+		);
 	}
 
 	sendFileContents() {
@@ -36,14 +55,14 @@ export class StaticContentRouter extends MiddleWare {
 			this.props.response.end(contents);
 
 		} catch(e) {
-			// console.log("File not found");
+			console.log("File not found");
 		}
 	}
 }
 
 StaticContentRouter.propTypes= {
 
-	public: PropTypes.string,
+	dir: PropTypes.string.isRequired,
 
 	hasPrefix: PropTypes.bool
 }
