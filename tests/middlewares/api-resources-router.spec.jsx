ad516d924303b9c363f3dacb4c0df92ee960a47c
Akshay Nair <phenax5@gmail.com>
2018-02-11 20:04:33 +0530

    Add test case for method matching

--- tests/middlewares/api-resources-router.spec.jsx
+++ tests/middlewares/api-resources-router.spec.jsx
@@ -13,24 +13,60 @@ import { mockCtx, createController } from '../../lib/testHelpers.jsx';
 
 describe('API Resources Router', () => {
 
-	describe('Resource class', () => {
+	describe('with simple handler function routing', () => {
 
 		it('should call the correct action in the controller', () => {
 
 			let handerFnWasCalled = false;
-			const handlerFn = () => { handerFnWasCalled = true; console.log('---------------------------'); }
+			const handlerFn = () => { handerFnWasCalled = true; }
 
-			const ctx = mockCtx('/posts', { method: 'get' });
+			const ctx = mockCtx('/users', { method: 'get' });
 
 			renderComponent(() =>
-				<Resource {...ctx} name='posts'>
+				<Resource {...ctx} name='users'>
 					<Action path='/' method='GET' handler={handlerFn} />
-					<Action path='/something-else' method='GET' handler={() => null} />
+					<Action path='/view' method='GET' handler={() => null} />
 				</Resource>
 			);
 
 			expect(handerFnWasCalled).to.be.true;
 		});
+
+
+		it('should not match when method is incorrect', () => {
+
+			let handerFnWasCalled = false;
+			const handlerFn = () => { handerFnWasCalled = true; }
+
+			const ctx = mockCtx('/users/add', { method: 'get' });
+
+			renderComponent(() =>
+				<Resource {...ctx} name='users'>
+					<Action path='/' method='GET' handler={() => null} />
+					<Action path='/add' method='POST' handler={handlerFn} />
+				</Resource>
+			);
+
+			expect(handerFnWasCalled).to.be.false;
+		});
+
+
+		// it('should call nothing when nothing matches', () => {
+
+		// 	let handerFnWasCalled = false;
+		// 	const handlerFn = () => { handerFnWasCalled = true; console.log('---------------------------'); }
+
+		// 	const ctx = mockCtx('/posts', { method: 'get' });
+
+		// 	renderComponent(() =>
+		// 		<Resource {...ctx} name='posts'>
+		// 			<Action path='/' method='GET' handler={handlerFn} />
+		// 			<Action path='/something-else' method='GET' handler={() => null} />
+		// 		</Resource>
+		// 	);
+
+		// 	expect(handerFnWasCalled).to.be.true;
+		// });
 	});
 
 
